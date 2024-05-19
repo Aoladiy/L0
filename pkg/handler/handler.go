5cabc511295ee03ba65d65a180b0ee57ba4c6151
Aldar <ochirov-3@mail.ru>
2024-05-19 18:05:01 +0300

    - basic http server

---
+++ pkg/handler/handler.go
@@ -0,0 +1,16 @@
+package handler
+
+import "github.com/gin-gonic/gin"
+
+type Handler struct {
+}
+
+func (h *Handler) InitRoutes() *gin.Engine {
+	router := gin.New()
+
+	api := router.Group("/api")
+	{
+		api.GET("orders/:id", h.getById)
+	}
+	return router
+}
